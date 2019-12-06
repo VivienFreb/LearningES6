e1fabedddcbeee70400bc200826391806540cdd8
VivienFreb <vivien.frebourg@outlook.com>
2019-12-06 17:14:48 +0100

    Promises, utilisation des then et catch.

--- promises.js
+++ promises.js
@@ -1,12 +1,12 @@
-let get = function (url){
-    return new Promise(function(resolve, reject){
+let get = function (url) {
+    return new Promise(function (resolve, reject) {
         let xhr = new window.XMLHttpRequest();
 
-        xhr.onreadystatechange = function(){
-            if(xhr.readyState === 4){
-                if(xhr.status === 200){
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === 4) {
+                if (xhr.status === 200) {
                     resolve(xhr.responseText);
-                } else{
+                } else {
                     reject(xhr);
                 }
             }
@@ -16,22 +16,19 @@ let get = function (url){
     })
 };
 
-let showError = function (e){
-    console.log('Erreur : ', e);
-};
-
-let getPosts = function(success, error){
-    return new Promise(function (resolve, reject){
-        get('https://jsonplaceholder.typicode.com/users').then(function(response){
-            let users = JSON.parse(response);
-            get('https://jsonplaceholder.typicode.com/posts?userId=' + users[0].id).then(function(response){
-                let posts = JSON.parse(response);
-                resolve(posts);
-            }).catch(showError)
-        }).catch(showError);
+let getPosts = function () {
+    return get('https://jsonplaceholder.typicode.com/users').then(function (response) {
+        let users = JSON.parse(response);
+        return get('https://jsonplaceholder.typicode.com/posts?userId=' + users[0].id)
+    }).then(function (response) {
+        return JSON.parse(response);
     })
 };
 
-getPosts().then(function(posts){
+getPosts().then(function (posts) {
     console.log('Le premier article : ', posts[0]);
+}).catch(function(error){
+    console.log(error)
+}).then(function(){
+    console.log('Fin des requêtes AJAX.')
 });
