5479ccdb3922f15e4b25d70c9e876295521f0b20
VivienFreb <vivien.frebourg@outlook.com>
2019-12-06 17:33:24 +0100

    Cas attente de plusieurs promises.

--- promises.js
+++ promises.js
@@ -32,3 +32,19 @@ getPosts().then(function (posts) {
 }).then(function(){
     console.log('Fin des requêtes AJAX.')
 });
+
+// Si on a plusieurs Promises et que l'on veut attendre que toutes les promesses soient faites, il existe Promise.all
+// cf. https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Objets_globaux/Promise/all
+// cf. https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Objets_globaux/Promise
+
+let promise1 = Promise.resolve(3);
+let promise2 = 42;
+let promise3 = new Promise(function(resolve, reject) {
+    setTimeout(resolve, 2000, 'foo');
+});
+// Au bout de 2 sec (le setTimeout), toutes les promesses seront effectuées
+Promise.all([promise1, promise2, promise3]).then(function(values) {
+    console.log(values);
+}).catch(function(error){
+    console.log('Une erreur : ', error);
+});
